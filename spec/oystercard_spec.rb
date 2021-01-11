0bf37447991e790323abd96ef2bc1e74144d3af3
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-01-11 15:34:58 +0000

    Added top_up method and passed RSpec test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,9 @@ describe OysterCard do
     expect(subject.balance).to eq 0
   end
 
+  it "should increase balance by 10" do
+    subject.top_up(10)
+    expect(subject.balance).to eq(10)
+  end
+
 end
